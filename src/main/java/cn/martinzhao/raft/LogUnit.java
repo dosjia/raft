740790d1f637957decbd5f843e3a79fd178f4abc
Martinez <martin.zhao1@homecredit.cn>
2021-10-15 17:55:59 +0800

    1. Add LogUnit object to store log.

---
+++ src/main/java/cn/martinzhao/raft/LogUnit.java
@@ -0,0 +1,17 @@
+package cn.martinzhao.raft;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Martin.Zhao
+ * @version 1.0
+ * @since 2021/10/15
+ */
+@Setter
+@Getter
+public class LogUnit<T> {
+    private int term;
+    private int index;
+    private T content;
+}
