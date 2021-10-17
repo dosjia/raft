a7e4410094ce43c4e079256280de8d9895e0c43d
Martinez <martin.zhao1@homecredit.cn>
2021-10-17 10:13:47 +0800

    1. Add object for peer node information.

---
+++ src/main/java/cn/martinzhao/raft/PeerNode.java
@@ -0,0 +1,16 @@
+package cn.martinzhao.raft;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Martin.Zhao
+ * @version 1.0
+ * @since 2021/10/15
+ */
+@Setter
+@Getter
+public class PeerNode {
+    private String address;
+    private int port;
+}
---
+++ src/main/java/cn/martinzhao/raft/PeerNodesInfo.java
@@ -0,0 +1,16 @@
+package cn.martinzhao.raft;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * @author Martin.Zhao
+ * @version 1.0
+ * @since 2021/10/15
+ */
+
+public class PeerNodesInfo {
+    public static List<PeerNode> peerNodes;
+}
