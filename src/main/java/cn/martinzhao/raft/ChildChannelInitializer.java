3221697f19f4cacaa3c002bfccd4b18ab7319cdb
Martinez <martin.zhao1@homecredit.cn>
2021-10-18 14:31:50 +0800

    1. Add handler to the pipeline.

--- src/main/java/cn/martinzhao/raft/ChildChannelInitializer.java
+++ src/main/java/cn/martinzhao/raft/ChildChannelInitializer.java
@@ -1,7 +1,16 @@
 package cn.martinzhao.raft;
 
+import cn.martinzhao.raft.coder.ByteToMessageDecoder;
+import cn.martinzhao.raft.coder.FormatEncoder;
+import cn.martinzhao.raft.coder.MessageToByteEncoder;
+import cn.martinzhao.raft.handler.CustomerReadIdleHandler;
+import cn.martinzhao.raft.handler.RequestProcessorHandler;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -11,7 +20,18 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class ChildChannelInitializer extends ChannelInitializer<SocketChannel> {
-    protected void initChannel(SocketChannel socketChannel) throws Exception {
+    private final static int MAX_LENGTH = 2042;
+    private final static ByteBuf DELIMITER = Unpooled.copiedBuffer(new byte[]{0x7e});
+    private int timeout = 10000;
+
+    protected void initChannel(SocketChannel ch) {
+        ch.pipeline().addLast(new DelimiterBasedFrameDecoder(MAX_LENGTH, DELIMITER));
+        ch.pipeline().addLast(new ByteToMessageDecoder());
+        ch.pipeline().addLast(new IdleStateHandler(timeout, 0, 0));
+        ch.pipeline().addLast(new CustomerReadIdleHandler());
+        ch.pipeline().addLast(new RequestProcessorHandler());
+        ch.pipeline().addLast(new FormatEncoder());
+        ch.pipeline().addLast(new MessageToByteEncoder());
         log.info("init here");
     }
 }
