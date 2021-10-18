518e477835bc1348f17a06f54fb57e22149d11c3
Martinez <martin.zhao1@homecredit.cn>
2021-10-18 14:35:41 +0800

    1. Add handler to command processor.

---
+++ src/main/java/cn/martinzhao/raft/handler/RequestProcessorHandler.java
@@ -0,0 +1,16 @@
+package cn.martinzhao.raft.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author Martin.Zhao
+ * @version 1.0
+ * @since 2021/10/18
+ */
+public class RequestProcessorHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+
+    }
+}
