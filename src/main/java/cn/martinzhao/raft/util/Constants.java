b65651e6e3126ae99063b68ecfd27d4cfb1fffd2
Martinez <martin.zhao1@homecredit.cn>
2021-10-18 15:32:29 +0800

    1. Add 2 commands.

--- src/main/java/cn/martinzhao/raft/util/Constants.java
+++ src/main/java/cn/martinzhao/raft/util/Constants.java
@@ -1,8 +1,9 @@
 package cn.martinzhao.raft.util;
 
 public class Constants {
-	public static byte IDENTIFY_BYTE = 0x7e;
-	public static byte IDENTIFY_BYTE_SECOND = 0x7d;
-
+    public static byte IDENTIFY_BYTE = 0x7e;
+    public static byte IDENTIFY_BYTE_SECOND = 0x7d;
+    public static byte[] CONNECTION_SETUP = {0x00, 0x01};
+    public static byte[] CONNECTION_SETUP_ANSWER = {(byte) 0x80, 0x01};
 
 }
