9853de3a95188a05c35a7f9720395cf9b661e497
Martinez <martin.zhao1@homecredit.cn>
2021-10-19 17:02:58 +0800

    1. Add constants for header.

--- src/main/java/cn/martinzhao/raft/util/Constants.java
+++ src/main/java/cn/martinzhao/raft/util/Constants.java
@@ -1,5 +1,8 @@
 package cn.martinzhao.raft.util;
 
+import cn.martinzhao.raft.bean.MessageHeader;
+import io.netty.util.AttributeKey;
+
 public class Constants {
     public static byte IDENTIFY_BYTE = 0x7e;
     public static byte IDENTIFY_BYTE_SECOND = 0x7d;
@@ -7,5 +10,9 @@ public class Constants {
     public static byte[] CONNECTION_SETUP_ANSWER = {(byte) 0x80, 0x01};
     public static byte[] REQUEST_VOTE = {0x00, 0x02};
     public static byte[] REQUEST_VOTE_ANSWER = {(byte) 0x80, 0x02};
+
+    public static final String MESSAGE_HEADER_KEY = "MessageHeader";
+    public static final AttributeKey<MessageHeader> MESSAGE_HEADER_ATTRIBUTE = AttributeKey
+            .valueOf(Constants.MESSAGE_HEADER_KEY);
 
 }
