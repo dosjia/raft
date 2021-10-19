3a69f3f8d2a50ff99754f2dfdeacee320a3b71b7
Martinez <martin.zhao1@homecredit.cn>
2021-10-19 13:16:52 +0800

    1. Fix Channel initializer bug.

--- src/main/java/cn/martinzhao/raft/channel/ClientChildChannelInitializer.java
+++ src/main/java/cn/martinzhao/raft/channel/ClientChildChannelInitializer.java
@@ -4,7 +4,7 @@ import cn.martinzhao.raft.coder.ByteToMessageDecoder;
 import cn.martinzhao.raft.coder.FormatEncoder;
 import cn.martinzhao.raft.coder.MessageToByteEncoder;
 import cn.martinzhao.raft.handler.CustomerReadIdleHandler;
-import cn.martinzhao.raft.handler.RequestProcessorHandler;
+import cn.martinzhao.raft.handler.ResponseProcessorHandler;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelInitializer;
@@ -21,13 +21,14 @@ public class ClientChildChannelInitializer extends ChannelInitializer<SocketChan
     private final static int MAX_LENGTH = 2042;
     private final static ByteBuf DELIMITER = Unpooled.copiedBuffer(new byte[]{0x7e});
     private int timeout = 10000;
+
     @Override
     protected void initChannel(SocketChannel ch) {
         ch.pipeline().addLast(new DelimiterBasedFrameDecoder(MAX_LENGTH, DELIMITER));
         ch.pipeline().addLast(new ByteToMessageDecoder());
         ch.pipeline().addLast(new IdleStateHandler(timeout, 0, 0));
         ch.pipeline().addLast(new CustomerReadIdleHandler());
-        ch.pipeline().addLast(new RequestProcessorHandler());
+        ch.pipeline().addLast(new ResponseProcessorHandler());
         ch.pipeline().addLast(new FormatEncoder());
         ch.pipeline().addLast(new MessageToByteEncoder());
     }
