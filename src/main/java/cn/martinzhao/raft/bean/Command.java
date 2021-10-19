a25c329c7d6ac9d910a491f86db137df01ff1d5a
Martinez <martin.zhao1@homecredit.cn>
2021-10-19 13:16:20 +0800

    1. Add 2 more commands.

--- src/main/java/cn/martinzhao/raft/bean/Command.java
+++ src/main/java/cn/martinzhao/raft/bean/Command.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
  */
 public enum Command {
     CONNECTION_SETUP(Constants.CONNECTION_SETUP), CONNECTION_SETUP_ANSWER(Constants.CONNECTION_SETUP_ANSWER),
+    REQUEST_VOTE(Constants.REQUEST_VOTE), REQUEST_VOTE_ANSWER(Constants.REQUEST_VOTE_ANSWER),
     ;
     private byte[] command;
 
