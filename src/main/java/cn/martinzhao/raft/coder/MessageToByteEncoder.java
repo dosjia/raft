82ac2135a0bdd9d49a902e20d43b1e6a9a7c9ab2
Martinez <martin.zhao1@homecredit.cn>
2021-10-19 13:10:23 +0800

    1. Add business logic to processing message object.

--- src/main/java/cn/martinzhao/raft/coder/MessageToByteEncoder.java
+++ src/main/java/cn/martinzhao/raft/coder/MessageToByteEncoder.java
@@ -1,14 +1,13 @@
 package cn.martinzhao.raft.coder;
 
 import cn.martinzhao.raft.bean.Message;
-import cn.martinzhao.raft.util.ByteUtil;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 /**
@@ -16,17 +15,24 @@ import java.util.List;
  * @version 1.0
  * @since 2021/10/18
  */
+@Slf4j
 public class MessageToByteEncoder extends MessageToMessageEncoder<Message> {
-    private final static Logger LOGGER = LoggerFactory.getLogger(MessageToByteEncoder.class);
 
     @Override
     protected void encode(ChannelHandlerContext ctx, Message msg, List<Object> out) {
-
         ByteBuf sendBuf = Unpooled.buffer();
-        byte[] tempArray = ByteUtil.shortToBytes(msg.getHeader().getCommandId());
+        byte[] tempArray = msg.getHeader().getCommandId().value();
         sendBuf.writeBytes(tempArray);// Add command ID
-
+        byte[] machineNameByte = msg.getHeader().getMachineName().getBytes(StandardCharsets.UTF_8);
+        sendBuf.writeShort(machineNameByte.length);
+        sendBuf.writeBytes(machineNameByte);
+        sendBuf.writeShort(msg.getBody().length);
+        sendBuf.writeBytes(msg.getBody());
+        tempArray = new byte[sendBuf.readableBytes()];
+        sendBuf.readBytes(tempArray);
+        sendBuf = Unpooled.buffer(tempArray.length);
+        sendBuf.writeBytes(tempArray);
         out.add(sendBuf);
-        LOGGER.debug("Write return message. Message is " + msg.getHeader().toString());
+        log.debug("Write return message.");
     }
 }
