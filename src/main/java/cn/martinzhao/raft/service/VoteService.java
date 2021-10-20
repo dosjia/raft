e3712bba758be7da080c577208a7051a49ec1fc0
Martinez <martin.zhao1@homecredit.cn>
2021-10-20 11:39:46 +0800

    1. Fix the NullPointer issue.

--- src/main/java/cn/martinzhao/raft/service/VoteService.java
+++ src/main/java/cn/martinzhao/raft/service/VoteService.java
@@ -3,7 +3,7 @@ package cn.martinzhao.raft.service;
 import cn.martinzhao.raft.bean.Command;
 import cn.martinzhao.raft.bean.LogUnit;
 import cn.martinzhao.raft.bean.Message;
-import cn.martinzhao.raft.bean.VoteResult;
+import cn.martinzhao.raft.bean.so.VoteResult;
 import cn.martinzhao.raft.bean.so.RequestForVoteRequestBody;
 import cn.martinzhao.raft.global.LocalCache;
 import cn.martinzhao.raft.global.NodeData;
@@ -24,9 +24,15 @@ public class VoteService {
             lock.unlock();
             return VoteResult.builder().term(NodeData.currentTerm.get()).success(false).build();
         }
-        LogUnit<Integer> lastLog = NodeData.logs.get(NodeData.logs.size() - 1);
-        if ((StringUtil.isNullOrEmpty(NodeData.votedFor) || NodeData.votedFor.equals(candidate)) && lastLogTerm >= lastLog.getTerm() && lastLogIndex >=
-                lastLog.getIndex()) {
+        LogUnit<Integer> lastLog;
+        if (NodeData.logs == null || NodeData.logs.isEmpty()) {
+            lastLog = null;
+        } else {
+            lastLog = NodeData.logs.get(NodeData.logs.size() - 1);
+        }
+        if ((StringUtil.isNullOrEmpty(NodeData.votedFor) || NodeData.votedFor.equals(candidate)) &&
+                lastLogTerm >= (lastLog == null ? 0 : lastLog.getTerm()) &&
+                lastLogIndex >= (lastLog == null ? 0 : lastLog.getIndex())) {
             NodeData.votedFor = candidate;
             lock.unlock();
             return VoteResult.builder().success(true).term(NodeData.currentTerm.get()).build();
@@ -37,9 +43,14 @@ public class VoteService {
 
     public void requestToOtherNodes() {
         Message message = new Message(Command.REQUEST_VOTE);
-        message.getHeader().setMachineName(NodeData.machineId);
-        LogUnit<Integer> lastLog = NodeData.logs.get(NodeData.logs.size() - 1);
-        NodeData.votedFor = NodeData.machineId;
+        message.getHeader().setMachineName(NodeData.MACHINE_ID);
+        LogUnit<Integer> lastLog;
+        if (NodeData.logs == null || NodeData.logs.isEmpty()) {
+            lastLog = null;
+        } else {
+            lastLog = NodeData.logs.get(NodeData.logs.size() - 1);
+        }
+        NodeData.votedFor = NodeData.MACHINE_ID;
         RequestForVoteRequestBody body = RequestForVoteRequestBody.builder().term(NodeData.currentTerm.incrementAndGet())
                 .indexOfLastLog(lastLog == null ? 0 : lastLog.getIndex()).termOfLastLog(lastLog == null ? 0 : lastLog
                         .getTerm()).build();
