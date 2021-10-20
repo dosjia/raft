4ba0e497523266fac300edc19a7a7087702ba6a5
Martinez <martin.zhao1@homecredit.cn>
2021-10-20 11:27:49 +0800

    1. implement the ISocketOutput interface.

--- src/main/java/cn/martinzhao/raft/bean/so/VoteResult.java
+++ src/main/java/cn/martinzhao/raft/bean/so/VoteResult.java
@@ -1,6 +1,5 @@
-package cn.martinzhao.raft.bean;
+package cn.martinzhao.raft.bean.so;
 
-import cn.martinzhao.raft.bean.so.ISocketInput;
 import cn.martinzhao.raft.util.ByteUtil;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,7 +13,7 @@ import java.util.Arrays;
  */
 @Builder
 @Getter
-public class VoteResult implements ISocketInput {
+public class VoteResult implements ISocketInput, ISocketOutput {
     int term;
     boolean success;
 
@@ -22,5 +21,17 @@ public class VoteResult implements ISocketInput {
     public void parseFromBytes(byte[] bytes) {
         this.term = ByteUtil.byteArrayToInt(Arrays.copyOfRange(bytes, 0, 4));
         this.success = (bytes[4] == (short) 1);
+    }
+
+    @Override
+    public byte[] parseToBytes() {
+        byte[] bytes = new byte[5];
+        byte[] temp = ByteUtil.intToByteArray(term);
+        bytes[0] = temp[0];
+        bytes[1] = temp[1];
+        bytes[2] = temp[2];
+        bytes[3] = temp[3];
+        bytes[4] = success ? 0x0 : (byte) 0x01;
+        return bytes;
     }
 }
